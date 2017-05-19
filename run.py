d1356ec9d22d08a87aed74af22a5d518d7125d9a
Dmitriy Yushko <yushko07@gmail.com>
2017-05-19 18:24:45 +0300

    Create new branch.

--- run.py
+++ run.py
@@ -1,76 +1,93 @@
-from tornado.web import RequestHandler, Application
-from tornado.websocket import WebSocketHandler
-from tornado.ioloop import IOLoop
-from tornado.options import options
-
-
 """
 Рабочий http и websocket python сервер
 Сервак взятый для примера: https://github.com/alxpy/simple-tornado-chat/blob/0.2/chat.py
 made by: Дмитрий Юшко
 """
 
-
-class BaseHandler(RequestHandler):
-	"""docstring for BaseHandler"""
-	def get(self):
-		self.write('Hello, World!')
-
-
-class TestHandler(BaseHandler):
-	"""docstring for TestHandler"""
-	def get(self):
-		self.write('Тут пока пусто...')
+from tornado.web import RequestHandler, Application
+from tornado.websocket import WebSocketHandler
+from tornado.ioloop import IOLoop
+from tornado.options import options
+from peewee import *
+from playhouse.sqlite_ext import SqliteExtDatabase
 
 
-class ChatHandler(BaseHandler):
-	"""docstring for Chat"""
-	def get(self):
-		context = dict(title='Main')
-		self.render('index.html', **context)
+DATABASE = 'server.db'
+
+
+db = SqliteExtDatabase(DATABASE)
+
+
+class MainPage(RequestHandler):
+    def get(self):
+        self.render('index.html', title='Тест')
+
+
+class User(Model):
+    id = PrimaryKeyField()
+    username = CharField(unique=True)
+
+    class Meta:
+        database = db
 
 
 class EchoWebSocket(WebSocketHandler):
-	"""docstring for EchoWebSocket"""
-	connections = set()
+    user_list= list()
+    connections = set()
 
-	def open(self):
-		self.connections.add(self)
+    def open(self):
+        self.connections.add(self)
 
-	def on_close(self):
-		self.connections.remove(self)
+    def on_close(self):
+        self.connections.remove(self)
 
-	def on_message(self, message):
-		print('New message:', message)
-		for conn in self.connections:
-			conn.write_message('You message: %s\n' % message)
+    def on_message(self, message):
+        import json
+        json_obj = json.loads(message)
+
+        self.write_message(json.dumps({'response':'FUCK!'}))
+        if 'nickname' in json_obj:
+            user = User.create(username=json_obj['nickname'])
+
+            self.user_list.append(json_obj['nickname'])
+
+            dump = json.dumps(dict(
+                user_id=user.id,
+                users=self.user_list,
+            ))
+
+            self.write_message(dump)
 
 
 def main():
-	import os
-	
-	options.parse_command_line()
-	
-	settings = dict(
-		debug=True,
-		template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-		static_path=os.path.join(os.path.dirname(__file__), 'static'),
-	)
-	
-	app = Application([
-		(r'/', BaseHandler),
-		(r'/log', TestHandler),
-		(r'/chat', ChatHandler),
-		(r'/ws', EchoWebSocket),
-	], **settings)
-	
-	app.listen(8000)
-	
-	try:
-		IOLoop.instance().start()
-	except:
-		IOLoop.instance().stop()
+    import os
+
+    db.connect()
+
+    if not os.path.exists('server.db'):
+        db.create_tables([User])
+
+    options.parse_command_line()
+
+    settings = dict(
+        debug=True,
+        template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+        static_path=os.path.join(os.path.dirname(__file__), 'static'),
+    )
+
+    app = Application([
+        (r'/', MainPage),
+        (r'/ws', EchoWebSocket),
+    ], **settings)
+
+    app.listen(8000)
+
+    try:
+        IOLoop.instance().start()
+    finally:
+        IOLoop.instance().stop()
+        db.close()
 
 
 if __name__ == '__main__':
-	main()
+    main()
